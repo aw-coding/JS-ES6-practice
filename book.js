ef0f8924f256ce65a6c4637a62ee2b972fb44bfc
Andrew Wood <andrewwood90@hotmail.com>
2021-02-26 15:24:45 +0000

    changed Book to class-constructor

---
+++ book.js
@@ -0,0 +1,15 @@
+// const Book = function (author, title, genre) {
+//   this.author = author;
+//   this.title = title;
+//   this.genre = genre;
+// };
+
+let Book = class{
+  constructor(author, title, genre){
+    this.author = author;
+    this.title = title;
+    this.genre = genre
+  }
+};
+
+module.exports = Book;
